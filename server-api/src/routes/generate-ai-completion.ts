eca415209df93d75a26f1250fb5b4103b00f2e56
Alan Vasconcelos <alandbh@gmail.com>
2023-09-13 21:26:02 -0300

    adding OpenAI completion

---
+++ server-api/src/routes/generate-ai-completion.ts
@@ -0,0 +1,52 @@
+import "dotenv/config"
+import { FastifyInstance } from "fastify";
+import {z} from 'zod' // Validação
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+
+export async function generateAiCompletionRoute(app:FastifyInstance) {
+    app.post('/ai/complete', async (req, res) => {
+        const bodySchema = z.object({
+            videoId: z.string().uuid(),
+            template: z.string(),
+            temperature: z.number().min(0).max(1).default(.5)
+        })
+
+        const {videoId, template, temperature} = bodySchema.parse(req.body)
+
+        const video  = await prisma.video.findUniqueOrThrow({
+            where: {
+                id: videoId
+            }
+        })
+
+        if (!video.transcription) {
+            return res.status(400).send({error: "Video transcript not found"})
+        }
+
+        const promptMessage = template.replace('{transcription}', video.transcription)
+
+        /**
+         * Sobre a escolha do modelo
+         * 
+         * O modelo gpt-3.5-turbo aceita no máximo 4096 tokens, incluindo o prompt e a resposta.
+         * Então, como a nossa descrição é muito grande, a gente iria gastar quase toda a cota apenas no prompt.
+         * Por isso, escolhemos o 'gpt-3.5-turbo-16k' que permite 16000 tokens.
+         * 
+         * Neste site é possível calcular a quantidade de tokens que a gente tá usando:
+         * https://platform.openai.com/tokenizer
+         */
+
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo-16k',
+            temperature,
+            messages: [{
+                role: 'user', content: promptMessage
+            }]
+        })
+
+        return response
+
+
+    })
+}
